06e3baaf2b0661b1fe98c64b6cd04f5627f60e84
Aaron Balleisen <aaronsb@princeton.edu>
2018-06-04 16:04:10 +0200

    push eliptical

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(eliptical)
+
+test_check("eliptical")
---
+++ tests/testthat/test_perimeter.R
@@ -0,0 +1,9 @@
+library(eliptical)
+library(testthat)
+
+context("Perimeter")
+
+test_that("perimeter of a circle", {
+  expect_equal(ellipse_perimeter(1), 2 * pi)
+  expect_equal(ellipse_perimeter(3), 2 * pi * 3)
+})
